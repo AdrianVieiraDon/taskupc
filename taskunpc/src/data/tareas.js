9195fc1882f454589e3fc153a42fc181386f1667
DonaVieira404 <vieiradonado@gmail.com>
2026-04-15 21:42:27 -0500

    creacion de datos mock para pruebas de la app

---
+++ taskunpc/src/data/tareas.js
@@ -0,0 +1,12 @@
+export const tareasIniciales = [
+  { id: 1, titulo: "Parcial de Cálculo", materia: "Matemáticas",
+    fecha: "2025-05-10", completada: false },
+  { id: 2, titulo: "Informe de Laboratorio", materia: "Física",
+    fecha: "2025-09-10", completada: true },
+  { id: 3, titulo: "Tarea de Ing.Software", materia: "Ing.Software",
+    fecha: "2025-08-11", completada: false },
+  { id: 4, titulo: "Proyecto de Aula", materia: "Programacion Web",
+    fecha: "2025-07-12", completada: true },
+  { id: 5, titulo: "Analisis numerico taller", materia: "Analisis numerico",
+    fecha: "2025-06-01", completada: false }
+];
